7abf2ca6df4450dfae59bd076774087dbb1718c3
Sura Sanjaya <surasanjaya@yahoo.com>
2024-08-04 00:44:33 +0700

    feat: add dummy handler function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("this is lacak lamaran")
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		w.Write([]byte("Hello world"))
+	}
+
+	http.HandleFunc("/", h)
+
+	log.Println("starting web server at http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("error when run the server")
+	}
 }
